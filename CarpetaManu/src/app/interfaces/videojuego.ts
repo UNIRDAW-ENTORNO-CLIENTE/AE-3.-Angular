94dbf6db21fafbf8a0a26adc360a1de7e88fd107
Manu <manu128477@gmail.com>
2023-12-04 16:48:56 +0100

    carpetaManu

---
+++ CarpetaManu/src/app/interfaces/videojuego.ts
@@ -0,0 +1,13 @@
+export interface Videojuego{
+    id: string,
+    titulo: string,
+    compania: string,
+    imagen: string, // falta poner esto
+    valoracionMedia: string
+}
+
+export interface MiVideojuego{
+    id: string,
+    titulo: string,
+    valoracionMedia: string
+}
